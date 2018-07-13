572f17d17078f6dd6b967c726ece233eaed35df7
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-07-13 18:23:45 +0200

    Implement reset

--- src/MockoDb.ts
+++ src/MockoDb.ts
@@ -4,6 +4,7 @@ import * as fsCallback from "fs";
 import { promisify } from "util";
 import { MongoClient } from "mongodb";
 import { URL } from "url";
+import { ListDatabasesResult } from "./types/mongodb";
 
 // Wrap in Promise
 const fs = {
@@ -38,14 +39,35 @@ export class MockoDb {
   constructor(private mongodHelper: MongodHelper, public url: URL) {}
 
   public async shutdown() {
-    const client = await MongoClient.connect(this.url.href);
-    await client
-      .db()
-      .executeDbAdminCommand({ shutdown: 1 })
-      .catch(err => {
-        const isShutdownError = /connection \d+ to .+ closed/.test(err.message);
-        if (!isShutdownError) throw err;
-      });
+    const client = await this.getClient();
+    const db = client.db();
+
+    await db.executeDbAdminCommand({ shutdown: 1 }).catch(err => {
+      const isShutdownError = /connection \d+ to .+ closed/.test(err.message);
+      if (!isShutdownError) throw err;
+    });
+  }
+
+  public async reset() {
+    const client = await this.getClient();
+    const db = client.db();
+
+    const result: ListDatabasesResult = await db.admin().listDatabases();
+    const allDbs = result.databases.map(db => db.name);
+
+    const toDrop = allDbs.filter(
+      name => !["admin", "config", "local"].includes(name)
+    );
+
+    await Promise.all(
+      toDrop.map(async dbName => {
+        await client.db(dbName).dropDatabase();
+      })
+    );
+  }
+
+  private async getClient() {
+    return MongoClient.connect(this.url.href);
   }
 }
 
